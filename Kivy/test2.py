8cece4544ee0bd295a4cab5f523f91dd01f07a42
Nick Foley <115027712+Foley-ops@users.noreply.github.com>
2023-07-12 17:08:48 -0500

    work on kivy

--- Kivy/test2.py
+++ Kivy/test2.py
@@ -6,46 +6,47 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from math import floor
 
-kivy.require('1.0.9')
+kivy.require("1.0.9")
+
 
 class CombatCalculator(GridLayout):
     def __init__(self, **kwargs):
         super(CombatCalculator, self).__init__(**kwargs)
         self.cols = 2
 
-        self.add_widget(Label(text='Hitpoints'))
+        self.add_widget(Label(text="Hitpoints"))
         self.hp = TextInput(multiline=False)
         self.add_widget(self.hp)
 
-        self.add_widget(Label(text='Attack'))
+        self.add_widget(Label(text="Attack"))
         self.attack = TextInput(multiline=False)
         self.add_widget(self.attack)
 
-        self.add_widget(Label(text='Strength'))
+        self.add_widget(Label(text="Strength"))
         self.strength = TextInput(multiline=False)
         self.add_widget(self.strength)
 
-        self.add_widget(Label(text='Defence'))
+        self.add_widget(Label(text="Defence"))
         self.defence = TextInput(multiline=False)
         self.add_widget(self.defence)
 
-        self.add_widget(Label(text='Ranged'))
+        self.add_widget(Label(text="Ranged"))
         self.ranged = TextInput(multiline=False)
         self.add_widget(self.ranged)
 
-        self.add_widget(Label(text='Prayer'))
+        self.add_widget(Label(text="Prayer"))
         self.prayer = TextInput(multiline=False)
         self.add_widget(self.prayer)
 
-        self.add_widget(Label(text='Magic'))
+        self.add_widget(Label(text="Magic"))
         self.magic = TextInput(multiline=False)
         self.add_widget(self.magic)
 
-        self.calculate = Button(text='Calculate')
+        self.calculate = Button(text="Calculate")
         self.calculate.bind(on_press=self.calculate_combat)
         self.add_widget(self.calculate)
 
-        self.output = Label(text='')
+        self.output = Label(text="")
         self.add_widget(self.output)
 
     def calculate_combat(self, instance):
@@ -63,11 +64,13 @@ class CombatCalculator(GridLayout):
         range_ = 0.325 * (floor(ranged / 2) + ranged)
         combat = floor(base + max(melee, mage, range_))
 
-        self.output.text = f'Combat Level: {combat:.2f}'
+        self.output.text = f"Combat Level: {combat:.2f}"
+
 
 class MyApp(App):
     def build(self):
         return CombatCalculator()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     MyApp().run()
